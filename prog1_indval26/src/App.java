9b7623c0c16ea5c19da1334015ba36a6bd08a3a7
dabsg <115629722+dabsg@users.noreply.github.com>
2026-08-25 12:31:22 +0200

    start

---
+++ prog1_indval26/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        int i =5;
+        String s="5";
+        
+        System.out.println("Hello, World! daniel"+i+"fgdfghfh"+s);
+        
+    }
+}
